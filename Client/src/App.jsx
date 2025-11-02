c9786aeffdaf6ef190a3e4fa27c9028ae960ffbf
govinda-mahanti <gmahanti955@gmail.com>
2025-11-02 23:41:42 +0530

    add

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -9,6 +9,7 @@ import Signup from "./Pages/Signup";
 import Cart from "./Pages/Cart";
 import Favourites from "./Pages/Favourites";
 import About from "./Pages/About";
+import Profile from "./Pages/Profile";
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
           <Route path="cart" element={<Cart />} />
           <Route path="/about" element={<About />} />
           <Route path="/favourites" element={<Favourites />} />
+          <Route path="/profile" element={<Profile />} />
         </Route>
       </Routes>
     </Router>
