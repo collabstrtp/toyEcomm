a3212d31d1e75efd2c2af8a515a0a23cf06bbca1
Sruti Prusty <srutiprusty03@gmail.com>
2026-02-28 02:01:04 +0530

    url

--- Client/src/Utils/urlconfig.jsx
+++ Client/src/Utils/urlconfig.jsx
@@ -1,1 +1,4 @@
-export const BASE_URL = "https://returntreasure-backend.vercel.app/api";
+export const BASE_URL = import.meta.env.VITE_APP_SERVER_BASE_URL;
+/*  "http://localhost:8080/api" ||
+  "https://returntreasure-backend.vercel.app/api";
+ */
